37ec4331485bc836a7d7bfac77868f2c32374f42
Consciencec <firec28@gmail.com>
2021-01-13 22:53:32 -0500

    attempt at fixing the scrolling issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,38 +36,6 @@
     $('.navbar-nav').onePageNav({
       currentClass: 'active'
     });
-
-    /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-        closedSymbol: '<i class="lni-chevron-right"></i>',
-        openedSymbol: '<i class="lni-chevron-down"></i>',
-      });
-
-    /* Back Top Link active
-    ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
-
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
-
   });      
 
 }(jQuery));
