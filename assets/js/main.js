751aa4ed5d06dd0c8b307fa78b53b32339cb3378
Consciencec <firec28@gmail.com>
2021-03-22 13:40:09 -0400

    removing css and js not in use

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,8 @@
+// page load animation (is this necessary for static content?)
 (function($) {
-  
   "use strict";  
-
   $(window).on('load', function() {
-
-  /*Page Loader active
-  ========================================================*/
-  $('#preloader').fadeOut();
-
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 100) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
-
-    /* Auto Close Responsive Navbar on Click
-    ========================================================*/
-    function close_toggle() {
-        if ($(window).width() <= 768) {
-            $('.navbar-collapse a').on('click', function () {
-                $('.navbar-collapse').collapse('hide');
-            });
-        }
-        else {
-            $('.navbar .navbar-inverse a').off('click');
-        }
-    }
-    close_toggle();
-    $(window).resize(close_toggle);
-
-    // one page navigation 
-    $('.navbar-nav').onePageNav({
-      currentClass: 'active'
-    });
+    // Page Loader active
+    $('#preloader').fadeOut();
   });      
-
 }(jQuery));
